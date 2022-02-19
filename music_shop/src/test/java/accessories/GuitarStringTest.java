6c72c2fbdf9dd4b04cfc89de31a6e3c5fc4257f1
petermordaunt <peetfire@hotmail.com>
2022-02-19 09:03:34 +0000

    Testing - Accessory Tests - via GuitarStringTest - test for hasBrand, hasProduct, hasCategory created - all passed. Function - created Accessory class.

--- music_shop/src/test/java/accessories/GuitarStringTest.java
+++ music_shop/src/test/java/accessories/GuitarStringTest.java
@@ -14,7 +14,7 @@ public class GuitarStringTest {
         strings = new GuitarString(2.99, 3.99, "Nice light gauge strings", "Ernie Ball", "Super Slinky", "Guitar Accessories", "Electric", "Extra Light");
     }
 
-    // tests for super super class 'StockItems' properties
+    // tests for super super class 'StockItem' properties
     @Test
     public void hasStockPrice(){
         assertEquals(2.99, strings.getStockPrice(), 0.0);
@@ -29,4 +29,21 @@ public class GuitarStringTest {
     public void hasDescription(){
         assertEquals("Nice light gauge strings", strings.getDescription());
     }
+    // Tests for super class Accessory properties and methods
+    @Test
+    public void hasBrand(){
+        assertEquals("Ernie Ball", strings.getBrand());
+    }
+
+    @Test
+    public void hasProduct(){
+        assertEquals("Super Slinky", strings.getProduct());
+    }
+
+    @Test
+    public void hasCategory(){
+        assertEquals("Guitar Accessories", strings.getCategory());
+    }
+
+
 }
