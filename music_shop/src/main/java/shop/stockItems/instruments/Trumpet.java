333d3f1879d1aa136a0c27f8fc7d35d00c7c5732
petermordaunt <peetfire@hotmail.com>
2022-02-19 08:33:11 +0000

    Testing - test for hasTrumpetType, hasMaterial and canPlay created - all passing.

--- music_shop/src/main/java/shop/stockItems/instruments/Trumpet.java
+++ music_shop/src/main/java/shop/stockItems/instruments/Trumpet.java
@@ -1,4 +1,24 @@
 package shop.stockItems.instruments;
 
-public class Trumpet {
+public class Trumpet extends Instrument{
+    private String trumpetType;
+    private String material;
+
+    public Trumpet(double stockPrice, double rrp, String description, String make, String model, String colour, String type, String trumpetType, String material) {
+        super(stockPrice, rrp, description, make, model, colour, type);
+        this.trumpetType = trumpetType;
+        this.material = material;
+    }
+
+    public String getTrumpetType() {
+        return trumpetType;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String play(){
+        return "Trumpet goes Toot Toot";
+    }
 }
--- music_shop/src/test/java/instruments/TrumpetTest.java
+++ music_shop/src/test/java/instruments/TrumpetTest.java
@@ -1,4 +1,31 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+import shop.stockItems.instruments.Trumpet;
+
+import static org.junit.Assert.assertEquals;
+
 public class TrumpetTest {
+    Trumpet trumpet;
+
+    @Before
+    public void before(){
+        trumpet = new Trumpet(300, 450.00, "Nice shiny trumpet", "Bach", "TR-501 S", "Brass", "Brass", "Bb Trumpet", "Brass");
+    }
+
+    @Test
+    public void hasTrumpetType(){
+        assertEquals("Bb Trumpet", trumpet.getTrumpetType());
+    }
+
+    @Test
+    public void hasMaterial(){
+        assertEquals("Brass", trumpet.getMaterial());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("Trumpet goes Toot Toot", trumpet.play());
+    }
 }
