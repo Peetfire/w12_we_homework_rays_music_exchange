0a048c79ea763561fdd806963d71da83bc797705
petermordaunt <peetfire@hotmail.com>
2022-02-19 08:07:15 +0000

    Testing - test for hasOboeTYpe, hasMaterial, canPlay and vanCalculateMarkup created - all passing.

--- music_shop/src/main/java/shop/stockItems/instruments/Oboe.java
+++ music_shop/src/main/java/shop/stockItems/instruments/Oboe.java
@@ -1,4 +1,29 @@
 package shop.stockItems.instruments;
 
-public class Oboe {
+public class Oboe extends Instrument {
+    private String oboeType;
+    private String material;
+
+    public Oboe(double stockPrice, double rrp, String description, String make, String model, String colour, String type, String oboeType, String material) {
+        super(stockPrice, rrp, description, make, model, colour, type);
+        this.oboeType = oboeType;
+        this.material = material;
+
+
+    }
+
+    public String getOboeType() {
+        return oboeType;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+
+    @Override
+    public String play() {
+        return "Oboe goes noodlely nood nood";
+    }
 }
+
--- music_shop/src/test/java/instruments/OboeTest.java
+++ music_shop/src/test/java/instruments/OboeTest.java
@@ -1,4 +1,36 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+import shop.stockItems.instruments.Oboe;
+
+import static org.junit.Assert.assertEquals;
+
 public class OboeTest {
+    Oboe oboe;
+
+    @Before
+    public void before(){
+        oboe = new  Oboe(300, 399.99, "Traditional hardwood tenor Oboe", "Yamaha", "YOB-241", "Black & Silver", "Woodwind", "Tenor","Rosewood & nickle silver");
+    }
+
+    @Test
+    public void hasOboeType(){
+        assertEquals("Tenor", oboe.getOboeType());
+    }
+
+    @Test
+    public void hasMaterial(){
+        assertEquals("Rosewood & nickle silver", oboe.getMaterial());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("Oboe goes noodlely nood nood", oboe.play());
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(99.99, oboe.calculateMarkup(), 0.001);
+    }
 }
