438d662205a08020617b0442f885e654f9ea5529
petermordaunt <peetfire@hotmail.com>
2022-02-20 09:01:15 +0000

    Refactoring - removed unneccesary StockItems class. Testing - reran all test - all passing.

--- music_shop/src/main/java/shop/stockItems/accessories/Accessory.java
+++ music_shop/src/main/java/shop/stockItems/accessories/Accessory.java
@@ -1,14 +1,21 @@
 package shop.stockItems.accessories;
 
+import shop.stockItems.ISell;
 import shop.stockItems.StockItem;
 
-public class Accessory extends StockItem {
+public class Accessory implements ISell {
+    private double stockPrice;
+    private double rrp;
+    private String description;
     private String brand;
     private String product;
     private String category;
 
+
     public Accessory(double stockPrice, double rrp, String description, String brand, String product, String category) {
-        super(stockPrice, rrp, description);
+        this.stockPrice = stockPrice;
+        this.rrp = rrp;
+        this.description = description;
         this.brand = brand;
         this.product = product;
         this.category = category;
@@ -26,6 +33,17 @@ public class Accessory extends StockItem {
         return category;
     }
 
+    public double getStockPrice() {
+        return stockPrice;
+    }
+
+    public double getRrp() {
+        return rrp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
     @Override
     public double calculateMarkup() {
         return this.getRrp() - this.getStockPrice();
--- music_shop/src/main/java/shop/stockItems/instruments/Instrument.java
+++ music_shop/src/main/java/shop/stockItems/instruments/Instrument.java
@@ -1,15 +1,21 @@
 package shop.stockItems.instruments;
 
+import shop.stockItems.ISell;
 import shop.stockItems.StockItem;
 
-public abstract class Instrument extends StockItem implements IPlay{
+public abstract class Instrument implements IPlay, ISell {
+    private double stockPrice;
+    private double rrp;
+    private String description;
     private String make;
     private String model;
     private String colour;
     private String type;
 
     public Instrument(double stockPrice, double rrp, String description, String make, String model, String colour, String type) {
-        super(stockPrice, rrp, description);
+        this.stockPrice = stockPrice;
+        this.rrp = rrp;
+        this.description = description;
         this.make = make;
         this.model = model;
         this.colour = colour;
@@ -30,6 +36,18 @@ public abstract class Instrument extends StockItem implements IPlay{
 
     public String getType() {
         return type;
+    }
+
+    public double getStockPrice() {
+        return stockPrice;
+    }
+
+    public double getRrp() {
+        return rrp;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     @Override
