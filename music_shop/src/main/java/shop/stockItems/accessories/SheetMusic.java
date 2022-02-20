46a6d5c98639d3de74cc50a1329e73a86ddf76fb
petermordaunt <peetfire@hotmail.com>
2022-02-20 08:27:10 +0000

    Testing - SheetMusicTest - tests for hasFormat and hasInstrumnet created - all passing. Function - created SheetMusic class.

--- music_shop/src/main/java/shop/stockItems/accessories/SheetMusic.java
+++ music_shop/src/main/java/shop/stockItems/accessories/SheetMusic.java
@@ -1,4 +1,20 @@
 package shop.stockItems.accessories;
 
-public class SheetMusic {
+public class SheetMusic extends Accessory{
+    private String format;
+    private String instrument;
+
+    public SheetMusic(double stockPrice, double rrp, String description, String brand, String product, String category, String format, String instrument) {
+        super(stockPrice, rrp, description, brand, product, category);
+        this.format = format;
+        this.instrument = instrument;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
 }
--- music_shop/src/test/java/accessories/SheetMusicTest.java
+++ music_shop/src/test/java/accessories/SheetMusicTest.java
@@ -1,4 +1,26 @@
 package accessories;
 
+import org.junit.Before;
+import org.junit.Test;
+import shop.stockItems.accessories.SheetMusic;
+
+import static org.junit.Assert.assertEquals;
+
 public class SheetMusicTest {
+    SheetMusic sheet;
+
+    @Before
+    public void before(){
+        sheet = new SheetMusic(10.00, 18.99, "Ultimate collection of Beatles songs", "Hal Leonard", "Ultimate Beatles", "Learning Accessories", "Collection / Songbook", "Piano" );
+    }
+
+    @Test
+    public void hasFormat(){
+        assertEquals("Collection / Songbook", sheet.getFormat());
+    }
+
+    @Test
+    public void hasInstrument(){
+        assertEquals("Piano", sheet.getInstrument());
+    }
 }
