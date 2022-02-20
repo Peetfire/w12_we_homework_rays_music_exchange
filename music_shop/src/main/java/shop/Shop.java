faa47b7d8b4cc911ffff8a4089477d052e07dda2
petermordaunt <peetfire@hotmail.com>
2022-02-20 21:18:31 +0000

    Testing - ShopTest - test for shopSTratsEmpty, canAddStock, canFindStock, canRemoveStock and can DEalWIthBadRemovalRequest created - all passing. Function - created Shop class.

--- music_shop/src/main/java/shop/Shop.java
+++ music_shop/src/main/java/shop/Shop.java
@@ -1,4 +1,39 @@
 package shop;
 
+import shop.stockItems.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+    private ArrayList<ISell> stock;
+
+    public Shop() {
+        this.stock = new ArrayList<>();
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
+    public int getStockCount() {
+        return stock.size();
+    }
+
+    public String addStock(ISell item){
+        if(this.stock.add(item)) return "Item added successfully!";
+        return "Item could not be added!";
+    }
+
+    public boolean findStock(ISell item){
+        return this.stock.contains(item);
+    }
+
+    public String removeStock(ISell item){
+        if(findStock(item)){
+            this.stock.remove(item);
+            return "Item removed successfully";
+        }
+        return "Item not found in stock";
+    }
+
 }
--- music_shop/src/test/java/ShopTest.java
+++ music_shop/src/test/java/ShopTest.java
@@ -1,2 +1,60 @@
+import org.junit.Before;
+import org.junit.Test;
+import shop.Shop;
+import shop.stockItems.ISell;
+import shop.stockItems.accessories.OboeReed;
+import shop.stockItems.instruments.Guitar;
+import shop.stockItems.instruments.Keyboard;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class ShopTest {
+    Shop shop;
+    ISell stockItem1, stockItem2, stockItem3;
+
+    @Before
+    public void before(){
+        stockItem1 = new Guitar(259.99, 359.99, "Stylish electric", "Gibson", "Firebird", "Fireburst", "Guitar", 6, "Electric");
+        stockItem2 = new OboeReed(9.00, 15.00, "Expertly handmade reed", "Barns & Mullins", "Jones Oboe Reed", "Woodwind Accessories", "Hand-finished", "Medium");
+        stockItem3 = new Keyboard(200.00, 270.50, "Lovely beginners keyboard",  "Roland", "MU1","Black", "Keys", "Electric Keyboard", 88);
+        shop = new Shop();
+    }
+    @Test
+    public void shopStartsEmpty(){
+        assertEquals(0, shop.getStockCount());
+    }
+
+    @Test
+    public void canAddStock(){
+        shop.addStock(stockItem1);
+        shop.addStock(stockItem2);
+        assertEquals(2, shop.getStockCount());
+    }
+
+    @Test
+    public void canFindStock(){
+        shop.addStock(stockItem1);
+        assertTrue(shop.findStock(stockItem1));
+    }
+
+    @Test
+    public void canRemoveStock(){
+        shop.addStock(stockItem1);
+        shop.addStock(stockItem2);
+        shop.addStock(stockItem3);
+        assertEquals(3, shop.getStockCount());
+        shop.removeStock(stockItem1);
+        assertEquals(2, shop.getStockCount());
+        shop.removeStock(stockItem2);
+        shop.removeStock(stockItem3);
+        assertEquals(0, shop.getStockCount());
+    }
+
+    @Test
+    public void canDealWithBadRemovalRequest(){
+        shop.addStock(stockItem1);
+        shop.removeStock(stockItem2);
+        assertEquals(1, shop.getStockCount());
+    }
 }
